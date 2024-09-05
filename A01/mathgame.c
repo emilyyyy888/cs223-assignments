f2cce4141bb716c74f76a0db83b2a6a846a2c2c7
emilyyyy888 <elu1@brynmawr.edu>
2024-09-05 14:06:40 -0400

    mathgame.c

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -5,8 +5,10 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  int rounds, num1, num2, answer, correctAns, correct = 0;
   printf("Welcome to Math Game!\n");
   printf("How many rounds do you want to play? ");
   scanf("%d", &rounds);
@@ -15,12 +17,13 @@ int main() {
     num1 = rand() % 9 + 1;
     num2 = rand() % 9 + 1;
     
-    prinf("%d + %d = ? ", num1, num2);
+    printf("%d + %d = ? ", num1, num2);
     scanf("%d", &answer);
 
     correctAns = num1 + num2;
     if (answer == correctAns){
       printf("Correct!\n");
+      correct++;
     } else {
       printf("Incorrect :(\n");
     }
