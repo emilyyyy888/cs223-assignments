6361e42d5afcb4b1576f2820e43cca3069ba7325
Emily Lu <elu1@goldengate.cs.brynmawr.edu>
2024-09-05 18:04:58 -0400

    Descriptive message

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,34 +1,46 @@
-/***************************************************
+/**
  * mathgame.c
- * Author: Emily Lu
- * Implements a math game
+ * Implements a simple math game that allows the user 
+ * to solve random addition problems.
+ * 
+ * @author: Emily Lu
+ * @version: September 5, 2024
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  int rounds, num1, num2, answer, correctAns, correct = 0;
-  printf("Welcome to Math Game!\n");
-  printf("How many rounds do you want to play? ");
-  scanf("%d", &rounds);
+    int rounds;        // Number of rounds to play
+    int num1, num2;    // Two random numbers 
+    int answer;        // The user's answer
+    int correctAns;    // Correct answer
+    int correct = 0;   // Count of correct answers
 
-  for (int i = 0; i < rounds; i++){
-    num1 = rand() % 9 + 1;
-    num2 = rand() % 9 + 1;
-    
-    printf("%d + %d = ? ", num1, num2);
-    scanf("%d", &answer);
+    printf("Welcome to Math Game!\n");
+    printf("How many rounds do you want to play? ");
+    scanf("%d", &rounds);
 
-    correctAns = num1 + num2;
-    if (answer == correctAns){
-      printf("Correct!\n");
-      correct++;
-    } else {
-      printf("Incorrect :(\n");
+    // Loop through the specified number of rounds
+    for (int i = 0; i < rounds; i++) {
+        num1 = rand() % 9 + 1;
+        num2 = rand() % 9 + 1;
+        
+        printf("%d + %d = ? ", num1, num2);
+        scanf("%d", &answer);
+
+        correctAns = num1 + num2;
+
+        // Check if the user's answer is correct
+        if (answer == correctAns) {
+            printf("Correct!\n");
+            correct++;  // Increment correct answer count
+        } else {
+            printf("Incorrect. The correct answer is %d.\n", correctAns);
+        }
     }
-  }
 
-  printf("You answered %d/%d correctly.\n", correct, rounds);
-  return 0;
+    printf("You answered %d/%d correctly.\n", correct, rounds);
+
+    return 0; 
 }
